b01098551c5787ef98ec2927a2b99ad8f96f189a
zahirul <zahirulopel@gmail.com>
2025-12-19 09:32:37 +0600

    add theme switcher from localstorage

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -2,10 +2,23 @@ import iconLightSun from "../assets/images/icon-sun-light.svg";
 import iconDarkSun from "../assets/images/icon-sun-dark.svg";
 import iconLightMoon from "../assets/images/icon-moon-light.svg";
 import iconDarkMoon from "../assets/images/icon-moon-dark.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ToggleTheme = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState<boolean>(() => {
+    const checkLocalStorage = localStorage.getItem("quizAppDarkTheme");
+    if (!checkLocalStorage) {
+      localStorage.setItem("quizAppDarkTheme", JSON.stringify(false));
+      return false;
+    }
+    const localStorageValue: boolean = JSON.parse(checkLocalStorage);
+
+    return localStorageValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("quizAppDarkTheme", JSON.stringify(isDark));
+  }, [isDark]);
 
   return (
     <div>
@@ -16,11 +29,12 @@ const ToggleTheme = () => {
           <img src={iconLightMoon} alt="light moon" />
         )}
         <input
-          onClick={() => setIsDark(!isDark)}
+          onChange={() => setIsDark(!isDark)}
           className="bg-brand-purple relative h-8 w-15 appearance-none rounded-2xl outline-none after:absolute after:top-[50%] after:left-[25%] after:h-6 after:w-6 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-white after:transition-[left] after:duration-300 after:content-[''] checked:after:left-[75%] hover:cursor-pointer"
           type="checkbox"
           name="toggle"
           id="toggle"
+          checked={isDark}
         />
         {isDark ? (
           <img src={iconDarkSun} alt="dark sun" />
