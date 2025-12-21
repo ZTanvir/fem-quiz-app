8255dc905c88974bcf125d26d6cf26d45187bfac
zahirul <zahirulopel@gmail.com>
2025-12-21 10:47:01 +0600

    add react router and quiz link added to home page

--- src/pages/Homepage/HomePage.tsx
+++ src/pages/Homepage/HomePage.tsx
@@ -1,5 +1,7 @@
 import useSWR from "swr";
 import type { Quiz } from "../../types";
+import ToggleTheme from "../../components/ToggleTheme";
+import { Link } from "react-router";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const Homepage = () => {
@@ -12,33 +14,38 @@ const Homepage = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <section>
-        <h1 className="flex flex-col text-4xl font-light">
-          Welcome to the{" "}
-          <span className="text-brand-mystic-navy font-bold">
-            Frontend Quiz!
-          </span>
-        </h1>
-        <p className="text-brand-gray-navy mt-4">
-          Pick a subject to get started.
-        </p>
-      </section>
-      <div className="space-y-4">
-        {data &&
-          data.map((quiz) => (
-            <div
-              className="flex cursor-pointer flex-wrap items-center gap-2 rounded-xl p-2 shadow-sm"
-              key={quiz.id}
-            >
-              <img
-                className="h-6 w-6 object-contain"
-                src={`src/${quiz.icon}`}
-                alt={`${quiz.title} icon`}
-              />
-              {quiz.title}
-            </div>
-          ))}
+    <div>
+      <div className="flex justify-end">
+        <ToggleTheme />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <section>
+          <h1 className="flex flex-col text-4xl font-light">
+            Welcome to the
+            <span className="text-brand-mystic-navy font-bold">
+              Frontend Quiz!
+            </span>
+          </h1>
+          <p className="text-brand-gray-navy mt-4">
+            Pick a subject to get started.
+          </p>
+        </section>
+        <div className="space-y-4">
+          {data &&
+            data.map((quiz) => (
+              <Link key={quiz.id} className="block" to="/quiz">
+                <div className="flex cursor-pointer flex-wrap items-center gap-2 rounded-xl p-2 shadow-sm">
+                  <img
+                    className="h-6 w-6 object-contain"
+                    src={`src/${quiz.icon}`}
+                    alt={`${quiz.title} icon`}
+                  />
+                  {quiz.title}
+                </div>
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   );
